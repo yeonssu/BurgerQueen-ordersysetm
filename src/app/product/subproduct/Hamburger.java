a8bf6ab027660fa0e2a7aec4e8071dea30e5865f
yeonssu <dustn7197@naver.com>
2023-01-02 12:26:15 +0900

    product class commit

---
+++ src/app/product/subproduct/Hamburger.java
@@ -0,0 +1,27 @@
+package app.product.subproduct;
+
+import app.product.Product;
+
+public class Hamburger extends Product {
+    private boolean isBurgerSet;
+    private int burgerSetPrice;
+    public Hamburger(int id, String name, int price, int kcal, boolean isBurgerSet, int burgerSetPrice) {
+        super(id, name, price, kcal);
+        this.isBurgerSet = isBurgerSet;
+        this.burgerSetPrice = burgerSetPrice;
+    }
+    public boolean isBurgerSet() {
+        return isBurgerSet;
+    }
+    public void setBurgerSet(boolean burgerSet) {
+        isBurgerSet = burgerSet;
+    }
+    public int getBurgerSetPrice() {
+        return burgerSetPrice;
+    }
+
+    public void setBurgerSetPrice(int burgerSetPrice) {
+        this.burgerSetPrice = burgerSetPrice;
+    }
+
+}
